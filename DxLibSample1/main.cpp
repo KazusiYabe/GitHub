df3b6d8f07d3fea8b473e7e927dfe5e1324b3484
KazusiYabe <hj-g20033@sist.ac.jp>
2021-05-14 12:32:19 +0900

    シーンの切り替えを頑張った

--- DxLibSample1/main.cpp
+++ DxLibSample1/main.cpp
@@ -1,79 +1,394 @@
 //ヘッダーファイルの読み込み
-#include "DxLib.h"  //DxLibを使う時は必要
+#include "DxLib.h"		//DxLibを使うときは必要
 
-#include "keyboard.h"
+#include "keyboard.h"	//キーボードの処理
+
 //マクロ定義
-#define GAME_TITLE "ゲームタイトル"
-#define GAME_WIDTH 1280
-#define GAME_HEIGHT 720
-#define GAME_COLOR 32
+#define GAME_TITLE	"ゲームタイトル"		//ゲームタイトル
+#define GAME_WIDTH	1280				//ゲーム画面の幅(ウィドス)
+#define GAME_HEIGHT	720					//ゲーム画面の高さ(ハイト)
+#define GAME_COLOR  32					//ゲームの色域
 
-#define GAME_ICON_ID  333
+#define GAME_ICON_ID	333				//ゲームのICONのID
 
-#define GAME_WINDOW_BAR 0
-// プログラムは は から始まります
-//Windowsのプログラム方法 = (WinAPI)で動いている
-//DxLibは、Directx という、ゲームプログラムを簡単に使える仕組み
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+#define GAME_WINDOW_BAR	0				//ウィンドウバーの種類
+
+//列挙型
+enum GAME_SCENE {
+	GAME_SCENE_TITLE,
+	GAME_SCENE_PLAY,
+	GAME_SCENE_END,
+	GAME_SCENE_CHANGE
+};	//ゲームのシーン
+
+//グローバル変数
+//シーンを管理する変数
+GAME_SCENE GameScene;		//現在のゲームのシーン
+GAME_SCENE OldGameScene;	//前回のゲームのシーン
+GAME_SCENE NextGameScene;	//次のゲームのシーン
+
+//画面の切り替え
+BOOL IsFadeOut = FALSE;	//フェードアウト
+BOOL IsFadeIn = FALSE;	//フェードイン
+
+int fadeTimeMill = 2000;
+int fadeTimeMax = fadeTimeMill / 1000 * 60;
+
+//フェードアウト
+int fadeOutCntInit = 0;
+int fadeOutCnt = fadeOutCntInit;
+int fadeOutCntMax = fadeTimeMax;
+
+//フェードイン
+int fadeInCntInit = fadeTimeMax;
+int fadeInCnt = fadeInCntInit;
+int fadeInCntMax = 0;
+
+//プロトタイプ宣言
+VOID Title(VOID);		//タイトル画面
+VOID TitleProc(VOID);	//タイトル画面(処理)
+VOID TitleDraw(VOID);	//タイトル画面(描画)
+
+VOID Play(VOID);		//プレイ画面
+VOID PlayProc(VOID);	//プレイ画面(処理)
+VOID PlayDraw(VOID);	//プレイ画面(描画)
+
+VOID End(VOID);			//エンド画面
+VOID EndProc(VOID);		//エンド画面(処理)
+VOID EndDraw(VOID);		//エンド画面(描画)
+
+VOID Change(VOID);		//切り替え画面
+VOID ChangeProc(VOID);	//切り替え画面(処理)
+VOID ChangeDraw(VOID);	//切り替え画面(描画)
+
+VOID ChangeScene(GAME_SCENE scene);  //シーン切り替え
+// プログラムは WinMain から始まります
+//Windowsのプログラミング方法 = (WinAPI)で動いている！
+//DxLibは、DirectXという、ゲームプログラミングをかんたんに使える仕組み
+int WINAPI WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nCmdShow)
 {
-	SetOutApplicationLogValidFlag(FALSE);   //Log.txtを出力しない
-	ChangeWindowMode(TRUE);
-	SetMainWindowText(GAME_TITLE);
-	SetGraphMode(GAME_WIDTH, GAME_HEIGHT, GAME_COLOR);
-	SetWindowSize(GAME_WIDTH, GAME_HEIGHT);
-	SetBackgroundColor(255, 255, 255);
-	SetWindowIconID(GAME_ICON_ID);
-	SetWindowStyleMode(GAME_WINDOW_BAR);     //ウィンドウバーの状態
-	SetWaitVSyncFlag(TRUE);                  //ディスプレイの垂直同期を有効にする
-	SetAlwaysRunFlag(TRUE);
+	SetOutApplicationLogValidFlag(FALSE);				//Log.txtを出力しない
+	ChangeWindowMode(TRUE);								//ウィンドウモードに設定
+	SetMainWindowText(GAME_TITLE);						//ウィンドウのタイトルの文字
+	SetGraphMode(GAME_WIDTH, GAME_HEIGHT, GAME_COLOR);	//ウィンドウの解像度を設定
+	SetWindowSize(GAME_WIDTH, GAME_HEIGHT);				//ウィンドウの大きさを設定
+	SetBackgroundColor(255, 255, 255);					//デフォルトの背景の色
+	SetWindowIconID(GAME_ICON_ID);						//アイコンファイルを読込
+	SetWindowStyleMode(GAME_WINDOW_BAR);				//ウィンドウバーの状態
+	SetWaitVSyncFlag(TRUE);								//ディスプレイの垂直同期を有効にする
+	SetAlwaysRunFlag(TRUE);								//ウィンドウをずっとアクティブにする
 
-	
-	if (DxLib_Init() == -1)		// ＤＸライブラリ初期化処理
+	//ＤＸライブラリ初期化処理
+	if (DxLib_Init() == -1)
 	{
-		return -1;			// エラーが起きたら直ちに終了
+		// エラーが起きたら直ちに終了
+		return -1;
 	}
-	
+
+	//ダブルバッファリング有効化
 	SetDrawScreen(DX_SCREEN_BACK);
 
-	//円の中心
+	//円の中心点
 	int X = GAME_WIDTH / 2;
 	int Y = GAME_HEIGHT / 2;
 	//円の半径
 	int radius = 50;
+
+	//最初のシーンは、タイトル画面から
+	GameScene = GAME_SCENE_TITLE;
+
 	//無限ループ
 	while (1)
 	{
-		//メッセージを受け取り続ける
-		if (ProcessMessage() != 0){break;  //無限ループを抜ける}
-		if (ClearDrawScreen() != 0) { break; }  //画面を消去する
+		if (ProcessMessage() != 0) { break; }	//メッセージを受け取り続ける
+		if (ClearDrawScreen() != 0) { break; }	//画面を消去する
 
 		//キーボード入力の更新
 		AllKeyUpdate();
-		
+
+		//ESCキーで強制終了
+		if (KeyClick(KEY_INPUT_ESCAPE) == TRUE) { break; }
+
+		//以前のシーンを取得
+		if (GameScene != GAME_SCENE_CHANGE)
+		{
+			OldGameScene = GameScene;
+		}
+
+		//シーンごとに処理を行う
+		switch (GameScene)
+		{
+		case GAME_SCENE_TITLE:
+			Title();			//タイトル画面
+			break;
+		case GAME_SCENE_PLAY:
+			Play();				//プレイ画面
+			break;
+		case GAME_SCENE_END:
+			End();				//エンド画面
+			break;
+		case GAME_SCENE_CHANGE:
+			Change();			//切り替え画面
+			break;
+		default:
+			break;
+		}
+
 		//キー入力
 		if (KeyDown(KEY_INPUT_UP) == TRUE)
 		{
-			Y--;  //上に移動
+			Y--;	//上に移動
 		}
 		if (KeyDown(KEY_INPUT_DOWN) == TRUE)
 		{
-			Y++;  //下に移動
+			Y++;	//下に移動
 		}
 		if (KeyDown(KEY_INPUT_LEFT) == TRUE)
 		{
-			X--;  //左に移動
+			X--;	//左に移動
 		}
 		if (KeyDown(KEY_INPUT_RIGHT) == TRUE)
 		{
-			X++;  //右に移動
+			X++;	//右に移動
 		}
+
 
 		DrawCircle(X, Y, radius, GetColor(255, 255, 0), TRUE);
 
-		ScreenFlip();
+		ScreenFlip();	//ダブルバッファリングした画面を描画
 	}
 
-	DxLib_End();				// ＤＸライブラリ使用の終了処理
 
-	return 0;				// ソフトの終了 
+	//ＤＸライブラリ使用の終了処理
+	DxLib_End();
+
+	return 0;	// ソフトの終了 
 }
+
+
+/// <summary>
+/// タイトル画面
+/// </summary>
+VOID Title(VOID)
+{
+	TitleProc();	//処理
+	TitleDraw();	//描画
+
+	return;
+}
+
+/// <summary>
+/// タイトル画面の処理
+/// </summary>
+VOID TitleProc(VOID)
+{
+	return;
+}
+
+/// <summary>
+/// タイトル画面の描画
+/// </summary>
+VOID TitleDraw(VOID)
+{
+
+	DrawString(0, 0, "タイトル画面", GetColor(0, 0, 0));
+	return;
+}
+
+/// <summary>
+/// プレイ画面
+/// </summary>
+VOID Play(VOID)
+{
+	PlayProc();	//処理
+	PlayDraw();	//描画
+
+	return;
+}
+
+/// <summary>
+/// プレイ画面の処理
+/// </summary>
+VOID PlayProc(VOID)
+{
+	if (KeyClick(KEY_INPUT_RETURN) == TRUE)
+	{
+		//シーン切り替え
+		//次のシーンの初期化をここで行うと楽
+
+		//プレイ画面に切り替え
+		ChangeScene(GAME_SCENE_END);
+	}
+	return;
+}
+
+/// <summary>
+/// プレイ画面の描画
+/// </summary>
+VOID PlayDraw(VOID)
+{
+	DrawString(0, 0, "プレイ画面", GetColor(0, 0, 0));
+	return;
+}
+
+/// <summary>
+/// エンド画面
+/// </summary>
+VOID End(VOID)
+{
+	EndProc();	//処理
+	EndDraw();	//描画
+
+	return;
+}
+
+/// <summary>
+/// エンド画面の処理
+/// </summary>
+VOID EndProc(VOID)
+{
+	if (KeyClick(KEY_INPUT_RETURN) == TRUE)
+	{
+		//シーン切り替え
+		//次のシーンの初期化をここで行うと楽
+
+		//プレイ画面に切り替え
+		ChangeScene(GAME_SCENE_TITLE);
+	}
+	return;
+}
+
+/// <summary>
+/// エンド画面の描画
+/// </summary>
+VOID EndDraw(VOID)
+{
+	DrawString(0, 0, "エンド画面", GetColor(0, 0, 0));
+	return;
+}
+
+/// <summary>
+/// 切り替え画面
+/// </summary>
+VOID Change(VOID)
+{
+	ChangeProc();	//処理
+	ChangeDraw();	//描画
+
+	return;
+}
+
+/// <summary>
+/// 切り替え画面の処理
+/// </summary>
+VOID ChangeProc(VOID)
+{
+	//フェードイン
+	if (IsFadeIn == TRUE)
+	{
+		if (fadeInCnt > fadeInCntMax)
+		{
+			fadeInCnt--;
+		}
+		else
+		{
+			//フェードイン処理が終わった
+
+			fadeInCnt = fadeInCntInit;
+			IsFadeIn = FALSE;
+		}
+	}
+	//フェードアウト
+	if (IsFadeIn == TRUE)
+	{
+		if (fadeInCnt < fadeInCntMax)
+		{
+			fadeInCnt++;
+		}
+		else
+		{
+			//フェードイン処理終わった
+
+			fadeInCnt = fadeInCntInit;
+			IsFadeOut = FALSE;
+		}
+	}
+	//切り替え処理終了
+	if (IsFadeIn == FALSE && IsFadeOut == FALSE)
+	{
+		//フェードインしていない、フェードアウトもしていないとき
+		GameScene = NextGameScene;
+		OldGameScene = GameScene;
+	}
+	return;
+}
+
+/// <summary>
+/// 切り替え画面の描画
+/// </summary>
+VOID ChangeDraw(VOID)
+{
+	//以前のシーンを描画
+	switch (OldGameScene)
+	{
+	case GAME_SCENE_TITLE:
+		TitleDraw();   //タイトル画面の描画
+		break;
+	case GAME_SCENE_PLAY:
+		TitleDraw();   //プレイ画面の描画
+		break;
+	case GAME_SCENE_END:
+		TitleDraw();   ////エンド画面の描画
+		break;
+	case GAME_SCENE_CHANGE:
+		TitleDraw();
+		break;
+	default:
+		break;
+	}
+
+	//シーンを切り替える
+	if (OldGameScene != GameScene)
+	{
+		//現在のシーンが切り替え画面でないとき
+		if (GameScene != GAME_SCENE_CHANGE)
+		{
+			NextGameScene = GameScene;
+			GameScene = GAME_SCENE_CHANGE;
+		}
+	}
+
+	//フェードイン
+	if (IsFadeIn == TRUE)
+	{
+		SetDrawBlendMode(DX_BLENDGRAPHTYPE_ALPHA, ((float)fadeInCnt / fadeInCntMax) * 255);
+		}
+
+	//フェードアウト
+	if (IsFadeOut == TRUE)
+	{
+		SetDrawBlendMode(DX_BLENDGRAPHTYPE_ALPHA, ((float)fadeInCnt / fadeInCntMax) * 255);
+	}
+
+	//四角を描画
+	DrawBox(0, 0, GAME_WIDTH, GAME_HEIGHT, GetColor(0, 0, 0), TRUE);
+
+	//半透明終了
+	SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+
+	DrawString(0, 0, "切り替え画面", GetColor(0, 0, 0));
+	return;
+}
+VOID ChangeScene(GAME_SCENE scene)
+{
+	GameScene = scene;  //プレイ画面に切り替え
+	IsFadeIn = FALSE;             //フェードインしない
+	IsFadeOut = TRUE;            // フェードアウトする
+
+	return;
+}
+
